f4c877939a6054e46a22ee7bbcdaed05367e8cdd
Cercel Irina <irinacercel@Irinas-MacBook-Pro.local>
2020-04-11 20:19:10 +0300

    hamburger menu

--- javascript/my_coins.js
+++ javascript/my_coins.js
@@ -5,3 +5,12 @@ document.getElementById('button-add').addEventListener('click', function () {
 document.querySelector('.close').addEventListener('click', function () {
     document.querySelector('.modal-add-coin').style.display = 'none'
 });
+
+function hamburgerMenuOnClick() {
+    document.getElementById("hamburger__menu").classList.toggle("change");
+    document.getElementsByClassName("navbar__items")[0].classList.toggle("change");
+    var elems = document.getElementsByClassName("navbar__item");
+    for(var i = 0; i<elems.length;i++) {
+        elems[i].classList.toggle("change");
+    }
+  } 
